ffefe59975c98e9201c9533ace58be28df615cfd
ivanBlade <itsesparta@gmail.com>
2023-06-13 09:41:56 -0500

    Agregando config server y config data

--- pintura/src/main/java/com/example/pintura/repository/PinturaRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.pintura.repository;
-
-import com.example.pintura.entity.Pintura;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PinturaRepository extends JpaRepository<Pintura, Integer>  {
-}
